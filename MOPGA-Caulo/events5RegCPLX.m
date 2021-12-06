6b629d4755cb8eddad0715b64d382735882c739a
daimm18 <56803789+daimm18@users.noreply.github.com>
2021-12-06 12:59:25 -0500

    Update events5RegCPLX.m

--- MOPGA-Caulo/events5RegCPLX.m
+++ MOPGA-Caulo/events5RegCPLX.m
@@ -1,10 +1,11 @@
-function [value,isterminal,direction] = events5RegCPLX(t, y)
 %&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%
 % This is the script for detecting specific events of the cell cycle.
 % This script is required by 'main5RegCPLX.m' for running.
 %&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&%
 % Locate the time when values passes through zero in a 
 % increasing direction and stop integration.
+
+function [value,isterminal,direction] = events5RegCPLX(t, y)
 global Ini Elong Zring;
 
 value=[sign(y(Ini) -  0.05); sign(y(Elong) - 0.2); sign(y(Elong) - 0.375); ...
